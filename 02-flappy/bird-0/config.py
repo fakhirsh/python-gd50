6265aafc24ecfaef5ad3f16f90b429ff6b934562
fakhirsh <fakhir.shaheen@gmail.com>
2024-02-10 12:10:14 +0500

    bird-0: loaded images

---
+++ 02-flappy/bird-0/config.py
@@ -0,0 +1,20 @@
+'''
+    Python port of the Harward's GD50 course
+    https://cs50.harvard.edu/games/2018/
+
+    Ported by:  Fakhir Shaheen
+    Website:    https://fakhirshaheen.com/
+    github:     https://github.com/fakhirsh
+    linkedin:   https://www.linkedin.com/in/fakhirshaheen/
+'''
+
+# Actual window dimensions
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+# Virtual resolution dimensions
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+# Desired framerate
+FPS = 60
