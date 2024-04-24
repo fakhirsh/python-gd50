d7a229b47730b1207011f7b5e1d1b8e2533927f6
fakhirsh <fakhir.shaheen@gmail.com>
2024-04-25 01:40:41 +0500

    dino HUD + Score

---
+++ 08-trex-scroller/04-movement/scenes/Dino.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class Dino : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (IsOnFloor()){
+			var runningColShape = GetNode<CollisionShape2D>("RunCol");
+			runningColShape.Disabled = false;
+
+			if (Input.IsActionPressed("dino_jump")){
+				velocity.Y = JumpVelocity;
+				var jumpSound = GetNode<AudioStreamPlayer2D>("JumpSound");
+				jumpSound.Play();
+			}
+			else if (Input.IsActionPressed("dino_duck")){
+				animatedSprite.Play("duck");
+				runningColShape.Disabled = true;
+			}
+			else{
+				animatedSprite.Play("run");
+			}
+		}
+		else{
+			velocity.Y += gravity * (float)delta;
+			animatedSprite.Play("jump");
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
---
+++ 08-trex-scroller/04-movement/scenes/Main.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public partial class Main : Node2D
+{
+	private Vector2 DINO_START_POS = new Vector2(150, 485);
+	private Vector2 CAM_START_POS = new Vector2(576, 324);
+	private Vector2 screen_size = new Vector2(0, 0);
+	private int score = 0;
+
+	private float speed;
+	private float START_SPEED = 200.0f;
+	private float MAX_SPEED = 700.0f;
+	// Called when the node enters the scene tree for the first time.
+	// Reference to Dino
+	private CharacterBody2D dino;
+	private Camera2D camera;
+	private ParallaxBackground bg;
+	private StaticBody2D ground;
+	public override void _Ready()
+	{
+		screen_size = GetViewport().GetVisibleRect().Size;
+		dino = GetNode<Dino>("Dino");
+		camera = GetNode<Camera2D>("Camera2D");
+		bg = GetNode<ParallaxBackground>("Background");
+		ground = GetNode<StaticBody2D>("Ground");
+		NewGame();
+	}
+
+	public void NewGame(){
+		score = 0;
+		
+		dino.Position = DINO_START_POS;
+		dino.Velocity = new Vector2(0, 0);
+		camera.Position = CAM_START_POS;
+		ground.Position = new Vector2(0, 0);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		speed = START_SPEED * (float)delta;
+		
+		dino.Position += new Vector2(speed, 0);
+		camera.Position += new Vector2(speed, 0);
+		
+		score += (int)speed;
+		GD.Print(score);
+		
+		if (camera.Position.X - ground.Position.X  > screen_size.X * 1.5){
+			ground.Position += new Vector2(screen_size.X, 0);
+		}
+	}
+}
